6ca8ad336c3e06bd3c42afa371533e564374b09f
Amir Dadipoor <amirdadipoor>
2025-05-11 23:20:09 +0330

    add support cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const taskRouter = require('./routes/taskRouter');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/tasks', taskRouter);
 
